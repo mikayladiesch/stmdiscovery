2512bff00ed3b3834161676566b9e973ac3b5591
mikayladiesch <mikayla@boulderes.com>
2020-04-24 16:19:31 -0600

    Removing unecessary files

--- STM32F4-Discovery_FW_V1.1.0_Makefiles/Project/Peripheral_Examples/Common.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-#
-# Common make file for STM32F4 Peripheral Examples
-#
-################################################################################
-STMFIRM=../../..
-SRC+= main.c
-SRC+= stm32f4xx_it.c
-include $(STMFIRM)/Project/Makefiles/Common.mk
--- STM32F4-Discovery_FW_V1.1.0_Makefiles/Project/STM32F4-Discovery-emb4fun/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-This project came from http://www.emb4fun.com/ by Michael Fischer.
-
-http://www.emb4fun.com/arm/examples/index.html
-
-Slightly modified to use LEDs.
